b569a8a4e8d637b49f5e41120f776bb5675f917f
Samantha Guergenenov <samguergen@gmail.com>
2017-03-30 11:53:08 -0400

    Update lm01/master from browser-template and js-template
    Refresh for cohort lm01

--- grunt/webpack-dev-server.js
+++ grunt/webpack-dev-server.js
@@ -1,27 +1,32 @@
-'use strict';
+'use strict'
 
-let clone = require('clone');
+const clone = require('clone')
 
 // clone the webpack config to separate configuration of webpack and dev server
-let webpackConfig = clone(require('./webpack').options);
+const webpackConfig = clone(require('./webpack').options)
 
-//port for webpack-dev-server
-const port = +('GA'.split('').reduce((p, c)=> p + c.charCodeAt(), ''));
+// port for development server
+const port = +('GA'.split('').reduce((p, c) => p + c.charCodeAt(), ''))
 
-// enable live reload without a script tag
-webpackConfig.entry.vendor.unshift(`webpack-dev-server/client?http://localhost:${port}`);
+// make `jQuery` and `$` available in the development console
+webpackConfig.module.loaders.push({
+  test: require.resolve('jquery'),
+  loader: 'expose?jQuery!expose?$'
+})
 
 module.exports = {
   options: {
     port,
+    inline: true, // reload on change
     webpack: webpackConfig,
+    publicPath: '/public/'
   },
 
   start: {
     keepAlive: true,
     webpack: {
       devtool: 'source-map',
-      debug: 'true',
-    },
-  },
-};
+      debug: 'true'
+    }
+  }
+}
